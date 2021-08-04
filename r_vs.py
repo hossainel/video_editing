d964115a018c144a00e29c4990f97758077ea607
Md Safayet El Hossain <31356820+hossainelsafayet@users.noreply.github.com>
2021-08-04 23:04:53 +0600

    Add files via upload

---
+++ r_vs.py
@@ -0,0 +1,17 @@
+
+# Import everything needed to edit video clips
+from moviepy.editor import VideoFileClip
+print('Video rotator By: tfaxmail@gmail.com')
+# loading video dsa gfg intro video
+vv = input('Enter File Location: ')
+if vv=='': exit()
+clip = VideoFileClip(vv)
+# rotating clip by 45 degree
+cc = int(input('Enter rotation in degree ([-] to rotate counter clockwize): '))
+if cc=='': cc=-90
+clip = clip.rotate(cc)
+oo = input('Enter output Filename: ')
+if oo=='':oo='output'
+else: oo=oo.replace('.mp4','')
+clip.write_videofile('%s.mp4'%oo, threads=8)
+print('All Done')
